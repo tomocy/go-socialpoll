81fdb90bfbe7092f9a6ae049cc60f2acf0b993d7
tomocy <tomocy.dev@gmail.com>
2018-08-16 03:10:13 +0900

    add sturct for mongdb

--- counter/db.go
+++ counter/db.go
@@ -1,6 +1,11 @@
 package main
 
-import mgo "gopkg.in/mgo.v2"
+import (
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type mongoDB struct {
 	url     string
@@ -21,4 +26,36 @@ func (db *mongoDB) dial() error {
 
 func (db *mongoDB) close() {
 	db.session.Close()
+}
+
+func (db mongoDB) updateCounts(counts map[string]int) {
+	polls := db.loadPolls()
+	for option, count := range counts {
+		selector := generateMongoDBSelector(option)
+		updater := generateMongoDBUpdater(option, count)
+		if _, err := polls.UpdateAll(selector, updater); err != nil {
+			log.Printf("failed to update the count of %s: %s\n", option, err)
+			continue
+		}
+	}
+}
+
+func (db mongoDB) loadPolls() *mgo.Collection {
+	return db.session.DB("ballots").C("polls")
+}
+
+func generateMongoDBSelector(option string) bson.M {
+	return bson.M{
+		"options": bson.M{
+			"$in": []string{option},
+		},
+	}
+}
+
+func generateMongoDBUpdater(option string, count int) bson.M {
+	return bson.M{
+		"$inc": bson.M{
+			"results." + option: count,
+		},
+	}
 }
