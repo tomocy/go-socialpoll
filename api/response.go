216d10f12de8b46e95627752536f6c2172a6b846
tomocy <tomocy.dev@gmail.com>
2018-08-16 22:58:42 +0900

    add functions to respond

--- api/response.go
+++ api/response.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
+func respond(w http.ResponseWriter, status int, data interface{}) {
+	w.WriteHeader(status)
+	if data != nil {
+		encodeBody(w, data)
+	}
+}
+
+func respondErr(w http.ResponseWriter, status int, data ...interface{}) {
+	respond(w, status, map[string]interface{}{
+		"error": map[string]interface{}{
+			"message": fmt.Sprint(data...),
+		},
+	})
+}
 
 func encodeBody(w http.ResponseWriter, data interface{}) error {
 	return json.NewEncoder(w).Encode(data)
