4c05a818ba8b3bb6beaffc8e561a863d6089c4ed
tomocy <tomocy.dev@gmail.com>
2018-08-16 21:59:06 +0900

    add functions for vars

--- api/main.go
+++ api/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"sync"
+)
 
+func main() {
+}
+
+var (
+	varsLock sync.RWMutex
+	vars     map[*http.Request]map[string]interface{}
+)
+
+func openVars(r *http.Request) {
+	varsLock.Lock()
+	defer varsLock.Unlock()
+	if vars == nil {
+		vars = make(map[*http.Request]map[string]interface{})
+	}
+	vars[r] = map[string]interface{}{}
+}
+
+func closeVars(r *http.Request) {
+	varsLock.Lock()
+	defer varsLock.Unlock()
+	delete(vars, r)
+}
+
+func getVar(r *http.Request, key string) interface{} {
+	varsLock.RLock()
+	defer varsLock.RUnlock()
+	return vars[r][key]
+}
+
+func setVar(r *http.Request, key string, value interface{}) {
+	varsLock.Lock()
+	defer varsLock.Unlock()
+	vars[r][key] = value
 }
