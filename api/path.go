b7bf854adad5193d2b2db330a7a1426bf69bb28f
tomocy <tomocy.dev@gmail.com>
2018-08-16 23:11:44 +0900

    add methods to create new path and check if path instance has id

--- api/path.go
+++ api/path.go
@@ -1,6 +1,29 @@
 package main
 
+import "strings"
+
 type path struct {
 	path string
 	id   string
+}
+
+const pathSeparator = "/"
+
+func newPath(path string) *path {
+	trimmedPath := strings.Trim(path, pathSeparator)
+	pathPieces := strings.Split(trimmedPath, pathSeparator)
+	var id string
+	if 2 <= len(pathPieces) {
+		id = pathPieces[len(pathPieces)-1]
+		trimmedPath = strings.Join(pathPieces[:len(pathPieces)-1], pathSeparator)
+	}
+
+	return &path{
+		path: trimmedPath,
+		id:   id,
+	}
+}
+
+func (p path) hasID() bool {
+	return 1 <= len(p.id)
 }
